e0a7949ea2239c076e4bf2b34874fdc01930d7e4
Mindcube-Builder <17810255490@163.com>
2026-01-31 08:46:21 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 's3.siliconflow.cn',
-      },
-    ],
+  typescript: {
+    
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+
+    ignoreDuringBuilds: true,
   },
 };
 
 export default nextConfig;
-
